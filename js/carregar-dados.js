cdd3821a91cb33260e117a008c33c4196109e37b
Junior Hoffmann <39073945+junior-hoffmann@users.noreply.github.com>
2025-03-31 12:35:03 -0300

    Update carregar-dados.js
    Atualização de preços

--- js/carregar-dados.js
+++ js/carregar-dados.js
@@ -4,67 +4,67 @@ let dados = {
         "itens": [
             {
                 "descricao": "Ovo ao leite 60g",
-                "preco": "R$ 15,00",
+                "preco": "R$ 19,00",
                 "imagem": "./assets/chocolates/ovo-60g.webp"
             },
             {
                 "descricao": "Ovo ao leite 120g",
-                "preco": "R$ 29,00",
+                "preco": "R$ 38,00",
                 "imagem": "./assets/chocolates/ovo-120g.webp"
             },
             {
                 "descricao": "Ovo ao leite 300g",
-                "preco": "R$ 66,00",
+                "preco": "R$ 94,00",
                 "imagem": "./assets/chocolates/ovo-300g.webp"
             },
             {
                 "descricao": "Ovo ao leite 450g",
-                "preco": "R$ 101,00",
+                "preco": "R$ 141,00",
                 "imagem": "./assets/chocolates/ovo-450g.webp"
             },
             {
                 "descricao": "Ovo ao leite 700g",
-                "preco": "R$ 137,00",
+                "preco": "R$ 220,00",
                 "imagem": "./assets/chocolates/ovo-700g.webp"
             },
             {
                 "descricao": "Coelho 40g",
-                "preco": "R$ 9,00",
+                "preco": "R$ 15,00",
                 "imagem": "./assets/chocolates/coelho-40g.webp"
             },
             {
                 "descricao": "Coelho 80g",
-                "preco": "R$ 18,00",
+                "preco": "R$ 25,00",
                 "imagem": "./assets/chocolates/coelho-80g.webp"
             },
             {
                 "descricao": "Coelha 150g",
-                "preco": "R$ 32,00",
+                "preco": "R$ 51,00",
                 "imagem": "./assets/chocolates/coelha-150g.webp"
             },
             {
                 "descricao": "Coelho 200g",
-                "preco": "R$ 42,00",
+                "preco": "R$ 63,00",
                 "imagem": "./assets/chocolates/coelho-200g.webp"
             },
             {
                 "descricao": "Coração Amo Você 100g",
-                "preco": "R$ 21,00",
+                "preco": "R$ 28,00",
                 "imagem": "./assets/chocolates/coracao-amo-voce.webp"
             },
             {
                 "descricao": "Coração 130g",
-                "preco": "R$ 29,00",
+                "preco": "R$ 39,00",
                 "imagem": "./assets/chocolates/coracao-130g.webp"
             },
             {
                 "descricao": "Coração 280g",
-                "preco": "R$ 62,00",
+                "preco": "R$ 82,00",
                 "imagem": "./assets/chocolates/coracao-280g.webp"
             },
             {
                 "descricao": "Coração 550g",
-                "preco": "R$ 117,00",
+                "preco": "R$ 154,00",
                 "imagem": "./assets/chocolates/coracao-550g.webp"
             }
         ]
@@ -74,37 +74,37 @@ let dados = {
         "itens": [
             {
                 "descricao": "Ovo branco 120g",
-                "preco": "R$ 29,00",
+                "preco": "R$ 38,00",
                 "imagem": "./assets/chocolates/ovo-branco-120g.webp"
             },
             {
                 "descricao": "Ovo branco 300g",
-                "preco": "R$ 66,00",
+                "preco": "R$ 94,00",
                 "imagem": "./assets/chocolates/ovo-branco-300g.webp"
             },
             {
                 "descricao": "Ovo branco 450g",
-                "preco": "R$ 101,00",
+                "preco": "R$ 141,00",
                 "imagem": "./assets/chocolates/ovo-branco-450g.webp"
             },
             {
                 "descricao": "Ovo branco 700g",
-                "preco": "R$ 142,00",
+                "preco": "R$ 220,00",
                 "imagem": "./assets/chocolates/ovo-branco-700g.webp"
             },
             {
                 "descricao": "Coelho branco 30g",
-                "preco": "R$ 7,00",
+                "preco": "R$ 10,00",
                 "imagem": "./assets/chocolates/coelho-branco-30g.webp"
             },
             {
                 "descricao": "Coelho branco 200g",
-                "preco": "R$ 42,00",
+                "preco": "R$ 63,00",
                 "imagem": "./assets/chocolates/coelho-branco-200g.webp"
             },
             {
                 "descricao": "Coração branco 130g",
-                "preco": "R$ 29,00",
+                "preco": "R$ 39,00",
                 "imagem": "./assets/chocolates/coracao-branco-130g.webp"
             }
         ]
@@ -114,182 +114,172 @@ let dados = {
         "itens": [
             {
                 "descricao": "Ovo Gran Espressione 300g",
-                "preco": "R$ 66,00",
+                "preco": "R$ 94,00",
                 "imagem": "./assets/chocolates/ovo-granespressione.webp"
             },
             {
                 "descricao": "Ovo Cookies 350g",
-                "preco": "R$ 75,00",
+                "preco": "R$ 110,00",
                 "imagem": "./assets/chocolates/ovo-cookies.webp"
             },
             {
                 "descricao": "Ovo Drágeas 300g",
-                "preco": "R$ 67,00",
+                "preco": "R$ 94,00",
                 "imagem": "./assets/chocolates/ovo-drageas.webp"
             },
             {
                 "descricao": "Ovo Fantasia ao leite 150g",
-                "preco": "R$ 33,00",
+                "preco": "R$ 47,00",
                 "imagem": "./assets/chocolates/ovo-fantasia.webp"
             },
             {
                 "descricao": "Ovo Fantasia branco 150g",
-                "preco": "R$ 33,00",
+                "preco": "R$ 47,00",
                 "imagem": "./assets/chocolates/ovo-fantasia-branco.webp"
             },
             {
                 "descricao": "Ovo Truffado Pérola Negra 450g",
-                "preco": "R$ 95,00",
+                "preco": "R$ 141,00",
                 "imagem": "./assets/chocolates/ovo-truffado-perola-negra.webp"
             },
             {
                 "descricao": "Ovo Truffado Avelã 450g",
-                "preco": "R$ 95,00",
+                "preco": "R$ 141,00",
                 "imagem": "./assets/chocolates/ovo-truffado-avela.webp"
             },
             {
                 "descricao": "Ovo Doce de Leite 180g",
-                "preco": "R$ 41,00",
+                "preco": "R$ 57,00",
                 "imagem": "./assets/chocolates/ovo-doce-de-leite.webp"
             },
             {
                 "descricao": "Meio Ovo com Drágeas 200g",
-                "preco": "R$ 44,00",
+                "preco": "R$ 63,00",
                 "imagem": "./assets/chocolates/meio-ovo-drageas.png"
             },
             {
                 "descricao": "Barra Feliz Páscoa 100g",
-                "preco": "R$ 20,00",
+                "preco": "R$ 31,00",
                 "imagem": "./assets/chocolates/barra-feliz-pascoa.webp"
             },
             {
                 "descricao": "Drágeas Crocantes Mistas 200g",
-                "preco": "R$ 37,00",
+                "preco": "R$ 51,00",
                 "imagem": "./assets/chocolates/drageas-mista-200g.webp"
             },
             {
                 "descricao": "Cenoura com Drágeas 100g",
-                "preco": "R$ 20,00",
+                "preco": "R$ 32,00",
                 "imagem": "./assets/chocolates/cenoura-drageas.webp"
             },
             {
                 "descricao": "Pacote com Ovos 130g",
-                "preco": "R$ 34,00",
+                "preco": "R$ 44,00",
                 "imagem": "./assets/chocolates/pacote-com-ovos-130g.webp"
             },
             {
                 "descricao": "Pacote com Ovos 65g",
-                "preco": "R$ 17,00",
+                "preco": "R$ 23,00",
                 "imagem": "./assets/chocolates/pacote-com-ovos-65g.webp"
             },
             {
                 "descricao": "Coração com Bombons 300g",
-                "preco": "R$ 64,00",
+                "preco": "R$ 88,00",
                 "imagem": "./assets/chocolates/coracao-com-bombons.webp"
             },
             {
                 "descricao": "Chocolate Sortido Misto 200g",
-                "preco": "R$ 38,00",
+                "preco": "R$ 51,00",
                 "imagem": "./assets/chocolates/chocolate-sortido-misto-200g.webp"
             },
             {
                 "descricao": "Bombonier com Trufas 200g",
-                "preco": "R$ 53,00",
+                "preco": "R$ 70,00",
                 "imagem": "./assets/chocolates/bombonier-trufas-200g.webp"
             },
             {
-                "descricao": "Caixa Acrílica com Trufas 240g",
-                "preco": "R$ 67,00",
-                "imagem": "./assets/chocolates/caixa-acrilica.webp"
-            },
-            {
                 "descricao": "Caixa Bombons Licor 135g",
-                "preco": "R$ 45,00",
+                "preco": "R$ 62,00",
                 "imagem": "./assets/chocolates/cx-bombom-licor.webp"
             },
             {
                 "descricao": "Caixa Bombons 180g",
-                "preco": "R$ 45,00",
+                "preco": "R$ 62,00",
                 "imagem": "./assets/chocolates/cx-bombom-180g.webp"
             },
             {
                 "descricao": "Caixa Bombons 120g",
-                "preco": "R$ 29,00",
+                "preco": "R$ 39,00",
                 "imagem": "./assets/chocolates/cx-bombons-120g.webp"
             },
             {
                 "descricao": "Caixa Bombons 225g",
-                "preco": "R$ 54,00",
+                "preco": "R$ 71,00",
                 "imagem": "./assets/chocolates/cx-bombons-225g.webp"
             },
             {
                 "descricao": "Caixa Bombons 270g",
-                "preco": "R$ 71,00",
+                "preco": "R$ 93,00",
                 "imagem": "./assets/chocolates/cx-bombons-270g.png"
             },
             {
                 "descricao": "Caixa Barras 216g",
-                "preco": "R$ 49,00",
+                "preco": "R$ 65,00",
                 "imagem": "./assets/chocolates/cx-barra-216g.webp"
             },
             {
-                "descricao": "Caixa Barras 430g",
-                "preco": "R$ 93,00",
-                "imagem": "./assets/chocolates/cx-barra-432g.webp"
-            },
-            {
                 "descricao": "Kit Ferramentas 120g",
-                "preco": "R$ 35,00",
+                "preco": "R$ 47,00",
                 "imagem": "./assets/chocolates/kit-ferramentas.webp"
             },
             {
                 "descricao": "Kit Maquiagem 75g",
-                "preco": "R$ 33,00",
+                "preco": "R$ 44,00",
                 "imagem": "./assets/chocolates/kit-maquiagem.webp"
             },
             {
                 "descricao": "Choco Player 100g",
-                "preco": "R$ 25,00",
+                "preco": "R$ 32,00",
                 "imagem": "./assets/chocolates/choco-player.webp"
             },
             {
                 "descricao": "Pinguim ao leite 100g",
-                "preco": "R$ 25,00",
+                "preco": "R$ 33,00",
                 "imagem": "./assets/chocolates/pinguim.webp"
             },
             {
                 "descricao": "Pinguim branco 100g",
-                "preco": "R$ 25,00",
+                "preco": "R$ 33,00",
                 "imagem": "./assets/chocolates/pinguim-branco.webp"
             },
             {
                 "descricao": "Caixa Dinossauro 100g",
-                "preco": "R$ 25,00",
+                "preco": "R$ 33,00",
                 "imagem": "./assets/chocolates/caixa-dino.webp"
             },
             {
                 "descricao": "Caixa Ovo para Colorir 280g",
-                "preco": "R$ 72,00",
+                "preco": "R$ 95,00",
                 "imagem": "./assets/chocolates/ovo-para-colorir.webp"
             },
             {
                 "descricao": "Ovo Saulinho 280g",
-                "preco": "R$ 70,00",
+                "preco": "R$ 92,00",
                 "imagem": "./assets/chocolates/ovo-saulinho.webp"
             },
             {
                 "descricao": "Ovo Dinos 180g",
-                "preco": "R$ 66,00",
+                "preco": "R$ 73,00",
                 "imagem": "./assets/chocolates/ovo-dinos.webp"
             },
             {
                 "descricao": "Ovo da Fada 180g",
-                "preco": "R$ 66,00",
+                "preco": "R$ 73,00",
                 "imagem": "./assets/chocolates/ovo-fadas.webp"
             },
             {
                 "descricao": "Chocophone 70g",
-                "preco": "R$ 23,00",
+                "preco": "R$ 30,00",
                 "imagem": "./assets/chocolates/choco-phone.png"
             },
             // {
@@ -314,32 +304,32 @@ let dados = {
         "itens": [
             {
                 "descricao": "Cesta Páscoa 213g",
-                "preco": "R$ 56,00",
+                "preco": "R$ 74,00",
                 "imagem": "./assets/chocolates/cesta-213g.webp"
             },
             {
                 "descricao": "Cesta Páscoa 400g",
-                "preco": "R$ 103,00",
+                "preco": "R$ 135,00",
                 "imagem": "./assets/chocolates/cesta-400g.webp"
             },
             {
                 "descricao": "Cesta Páscoa Branca 460g",
-                "preco": "R$ 113,00",
+                "preco": "R$ 148,00",
                 "imagem": "./assets/chocolates/cesta-branca-460.webp"
             },
             {
                 "descricao": "Cesta com Alça 403g",
-                "preco": "R$ 108,00",
+                "preco": "R$ 142,00",
                 "imagem": "./assets/chocolates/cesta-com-alca.webp"
             },
             {
                 "descricao": "Cesta Páscoa 700g",
-                "preco": "R$ 186,00",
+                "preco": "R$ 243,00",
                 "imagem": "./assets/chocolates/cesta-700g.png"
             },
             {
                 "descricao": "Cesta Páscoa 1,73kg",
-                "preco": "R$ 406,00",
+                "preco": "R$ 542,00",
                 "imagem": "./assets/chocolates/cesta-1kg.webp"
             },
         ]
@@ -349,27 +339,27 @@ let dados = {
         "itens": [
             {
                 "descricao": "Ovo meio amargo 180g",
-                "preco": "R$ 41,00",
+                "preco": "R$ 57,00",
                 "imagem": "./assets/chocolates/ovo-meio-amargo-180g.webp"
             },
             {
                 "descricao": "Ovo meio amargo 300g",
-                "preco": "R$ 66,00",
+                "preco": "R$ 94,00",
                 "imagem": "./assets/chocolates/ovo-meio-amargo-300g.webp"
             },
             {
                 "descricao": "Chocolate Sabor meio amargo 50g",
-                "preco": "R$ 14,00",
+                "preco": "R$ 18,00",
                 "imagem": "./assets/chocolates/sabor-meio-amargo.webp"
             },
             {
                 "descricao": "Caixa de Trufas meio amargo 160g",
-                "preco": "R$ 40,00",
+                "preco": "R$ 58,00",
                 "imagem": "./assets/chocolates/trufas-meio-amargo.webp"
             },
             {
                 "descricao": "Coelha meio amargo 150g",
-                "preco": "R$ 31,00",
+                "preco": "R$ 47,00",
                 "imagem": "./assets/chocolates/coelha-meio-amargo.webp"
             }
         ]
@@ -379,12 +369,12 @@ let dados = {
         "itens": [
             {
                 "descricao": "Chocolate Sabor 70% cacau 50g",
-                "preco": "R$ 14,00",
+                "preco": "R$ 18,00",
                 "imagem": "./assets/chocolates/sabor-70.webp"
             },
             {
                 "descricao": "Ovo 70% cacau 300g",
-                "preco": "R$ 69,00",
+                "preco": "R$ 114,00",
                 "imagem": "./assets/chocolates/ovo-70-300g.webp"
             }
         ]
@@ -394,22 +384,22 @@ let dados = {
         "itens": [
             {
                 "descricao": "Caixa Barras diet 108g",
-                "preco": "R$ 29,00",
+                "preco": "R$ 39,00",
                 "imagem": "./assets/chocolates/caixa-diet-108.webp"
             },
             {
                 "descricao": "Caixa de Trufas diet 160g",
-                "preco": "R$ 40,00",
+                "preco": "R$ 58,00",
                 "imagem": "./assets/chocolates/trufas-diet.webp"
             },
             {
                 "descricao": "Ovo diet 120g",
-                "preco": "R$ 32,00",
+                "preco": "R$ 43,00",
                 "imagem": "./assets/chocolates/ovo-diet-120g.webp"
             },
             {
                 "descricao": "Chocolate Sabor diet 50g",
-                "preco": "R$ 14,00",
+                "preco": "R$ 18,00",
                 "imagem": "./assets/chocolates/sabor-diet.webp"
             },
             {
@@ -424,22 +414,22 @@ let dados = {
         "itens": [
             {
                 "descricao": "Coelha soja 150g",
-                "preco": "R$ 42,00",
+                "preco": "R$ 56,00",
                 "imagem": "./assets/chocolates/coelha-soja.webp"
             },
             {
                 "descricao": "Ovo soja 150g",
-                "preco": "R$ 42,00",
+                "preco": "R$ 56,00",
                 "imagem": "./assets/chocolates/ovo-soja-150g.webp"
             },
             {
                 "descricao": "Chocolate Sabor soja 50g",
-                "preco": "R$ 14,00",
+                "preco": "R$ 19,00",
                 "imagem": "./assets/chocolates/sabor-soja.webp"
             },
             {
                 "descricao": "Pacote Chocolate soja 100g",
-                "preco": "R$ 29,00",
+                "preco": "R$ 39,00",
                 "imagem": "./assets/chocolates/pacote-chocolate-soja.webp"
             }
         ]
